298c1266d28242cebd8d894ce44aed4486bc4c0d
sgrmhdk <31389818+SagarMahadik@users.noreply.github.com>
2023-02-24 15:09:06 +0530

    Successful build for constants and button
    Added constants and button packages

---
+++ packages/styles/src/index.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Button = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	padding: 8px 16px;
+	border-radius: 4px;
+	background-color: #000;
+	color: #fff;
+	font-size: 16px;
+	font-weight: 500;
+	cursor: pointer;
+	width: 100px;
+	height: 200px;
+`;
